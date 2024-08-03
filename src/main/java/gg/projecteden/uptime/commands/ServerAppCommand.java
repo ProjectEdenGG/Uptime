7700708eef3a9f76e251fc5ebe20be852fe9d3ef
GriffinCodes <griffin@projecteden.gg>
2024-08-02 22:34:43 -0500

    Actually run the thread

--- src/main/java/gg/projecteden/uptime/commands/ServerAppCommand.java
+++ src/main/java/gg/projecteden/uptime/commands/ServerAppCommand.java
@@ -24,29 +24,33 @@ public class ServerAppCommand extends AppCommand {
 
 	@Command("Start a server")
 	void start(@Optional("smp") String server) {
-		new Thread(() -> Utils.bash("mark2 start -n " + server, validate(server)));
+		final File folder = folder(server);
+		new Thread(() -> Utils.bash("mark2 start -n " + server, folder)).start();
 		replyEphemeral("Starting server " + server);
 	}
 
 	@Command("Stop a server")
 	void stop(@Optional("smp") String server) {
-		new Thread(() -> Utils.bash("mark2 send -n " + server + " maintenance", validate(server)));
+		final File folder = folder(server);
+		new Thread(() -> Utils.bash("mark2 send -n " + server + " maintenance", folder)).start();
 		replyEphemeral("Stopping server " + server);
 	}
 
 	@Command("Reboot a server")
 	void reboot(@Optional("smp") String server) {
-		new Thread(() -> Utils.bash("mark2 send -n " + server + " reboot", validate(server)));
+		final File folder = folder(server);
+		new Thread(() -> Utils.bash("mark2 send -n " + server + " reboot", folder)).start();
 		replyEphemeral("Rebooting server " + server);
 	}
 
 	@Command("Force stop a server")
 	void kill(@Optional("smp") String server) {
-		new Thread(() -> Utils.bash("mark2 kill -n " + server, validate(server)));
+		final File folder = folder(server);
+		new Thread(() -> Utils.bash("mark2 kill -n " + server, folder)).start();
 		replyEphemeral("Killing server " + server);
 	}
 
-	private static File validate(String server) {
+	private static File folder(String server) {
 		final Path path = Path.of("/home/minecraft/servers", server);
 		if (!Files.exists(path))
 			throw new AppCommandException("Server " + server + " does not exist");
